427ccc6d0679bca0e739972a5eb4630cc9a4cafb
Asma <as1588ma@gmail.com>
2021-09-01 20:34:15 -0400

    lesson 15 more objec

--- lesson14/main.js
+++ lesson14/main.js
@@ -1,7 +1,7 @@
 // DATA TYPES part 2
 //boolean
 // booleans are TRUE or FALSE
-
+/*
 const isEqual = 10 === 10;
 console.log({isEqual});
 const someName ="";
@@ -27,26 +27,29 @@ const bigObject = {
     }
 }
 console.log({bigObject})
-
+*/
 
 // a.b.c
 
-const threeLevelDeepObject = {
+/*const threeLevelDeepObject = {
     a:{
         b: {
             c: {
-                someProperty: "this is the value of the deepest property",
+                
+                
+                
+                    someProperty: "this is the value of the deepest property",
             }
             
         }
     }
 }
 console.log(threeLevelDeepObject)
-console.log(threeLevelDeepObject.a.b.c)
+console.log(threeLevelDeepObject.a.b.c)*/
 
 //Example
 
-const company = {
+/*const company = {
     name: "apple",
     isBankrupt: false,
     address: {
@@ -67,7 +70,7 @@ const company = {
             },
         },
         {
-            name:"Gypsy",
+                 name:"Gypsy",
             age: 30,
             salary: 9000,
             address: {
@@ -79,12 +82,151 @@ const company = {
         },
     ]  
 }
-console.log(company.employees[0].address.country);
+console.log(company.employees[0].address.country);*/
 
 //HomeWork
 //create an object that multiple properties.// value => arrays, nested objects, numbers, strings, booleans
+
+/*const jaimieClassStudentsHomework = {
+    name:"Myles",
+    homeworkSubmitted: true,
+    grades: "B+",
+    attendance: [
+        {
+            monday: true,
+            tuesday: true,
+            wednesday: false,
+            thursday: true,
+            friday: true,
+        }
+    ]
+}*/
+
+const jaimieClassStudents = {
+  students: [
+    {
+      name: "Myles",
+      homeworkSubmitted: true,
+      grades: "B+",
+      attendance: [
+        {
+          monday: true,
+          tuesday: true,
+          wednesday: false,
+          thursday: true,
+          friday: true,
+        },
+      ],
+      name: "bobby",
+      homeworkSubmitted: true,
+      grades: "C",
+      attendance: [
+        {
+          monday: true,
+          tuesday: false,
+          wednesday: false,
+          thursday: true,
+          friday: true,
+        },
+      ],
+      name: "Lily",
+      homeworkSubmitted: false,
+      grades: "B",
+      attendance: [
+        {
+          monday: true,
+          tuesday: true,
+          wednesday: true,
+          thursday: false,
+          friday: false,
+        },
+      ],
+    },
+  ],
+};
+const keys = Object.keys(jaimieClassStudents);
+for (key of keys) {
+  console.log({ key });
+}
+//console.log(jaimieClassStudents.name)
 //homework 2
 //create an object which contains 3 properties, and every property has some string as its value.
-//log every property of that object. separate. 
-//
-// can object have a Function?? 
+//log every property of that object. separate.
+
+const cars = {
+  favColor: "Black",
+
+  favCarNames: [
+    {
+      carName: "Toyota",
+      model: "Solara",
+      price: 10000,
+      color: "Blue",
+    },
+
+    {
+      carName: "Honda",
+      model: "Civic",
+      price: 12000,
+      color: "Red",
+    },
+    {
+      carName: "Fords",
+      model: "F150",
+      price: 20000,
+      color: "Black",
+    },
+  ],
+};
+for (currentCar of cars.favCarNames) {
+  //console.log(`(He bought a, ${cars.color + "  " + cars.carName[1]}.`);
+  console.log(`(He bought a, ${currentCar.color + " " + currentCar.carName}.`);
+}
+const numberOfFavCarNames = cars.favCarNames.length;
+numberOfFavCarNames;
+
+console.log(
+  `The name of the last car is - ${
+    cars.favCarNames[numberOfFavCarNames - 1].name
+  }`
+);
+
+// can object have a Function??
+
+const math = {
+  pi: 3.14,
+  multiply: (a, b) => {
+    return a * b;
+  },
+  subtract: (a, b) => {
+    return a - b;
+  },
+};
+console.log(math.pi);
+console.log(math.multiply(2, 10));
+
+const objectWithProperties = {
+  firstProperty: "first",
+  secondProperty: "second",
+  thirdProperty: "third",
+};
+for (property of Object.keys(objectWithProperties)) {
+  console.log({ property });
+}
+const entries = Object.entries(objectWithProperties);
+entries;
+for (entry of Object.entries(objectWithProperties)) {
+  const property = entry[0];
+  const value = entry[1];
+  console.log({ property, value });
+}
+
+//HOMEWORK
+//extract properties from the below object.
+//print out the left side of the object.
+const homeworkObject = {
+  firstKey: 10,
+  secondKey: 20,
+};
+for (const [keys, value] of Object.entries(homeworkObject)) console.log(keys);
+console.log({ homeworkObject });
